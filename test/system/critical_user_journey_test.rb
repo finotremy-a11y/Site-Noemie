118c11858f1b2e9b2a9ebec947a60c6b25701ae1
Remy Finot <finotremy@gmail.com>
2026-05-22 10:37:48 +0200

    Add regression tests for polished customer flows

--- test/system/critical_user_journey_test.rb
+++ test/system/critical_user_journey_test.rb
@@ -4,6 +4,8 @@ class CriticalUserJourneyTest < ApplicationSystemTestCase
   test "visitor can navigate critical pages" do
     visit root_path
     assert_text "Des burgers qui changent tout"
+    assert_text "Commander maintenant"
+    assert_text "4,9/5"
 
     click_link "Voir la carte"
     assert_current_path catalog_path
@@ -16,6 +18,7 @@ class CriticalUserJourneyTest < ApplicationSystemTestCase
     assert(has_text?("Finaliser ma commande") || has_text?("Notre Carte"))
 
     visit contact_path
-    assert_text "Contact"
+    assert_text "Réponse sous 24h ouvrées"
+    assert_text "1463 route d'avignon"
   end
 end
